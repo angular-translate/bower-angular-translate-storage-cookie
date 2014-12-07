549fd94d8069454987ed6057ad04ecb3f0199416
knalli <knallisworld@googlemail.com>
2014-12-07 18:40:33 +0100

    Release 2.5.0

--- angular-translate-storage-cookie.js
+++ angular-translate-storage-cookie.js
@@ -1,5 +1,5 @@
 /*!
- * angular-translate - v2.4.2 - 2014-10-21
+ * angular-translate - v2.5.0 - 2014-12-07
  * http://github.com/angular-translate/angular-translate
  * Copyright (c) 2014 ; Licensed MIT
  */
@@ -11,6 +11,9 @@ angular.module('pascalprecht.translate').factory('$translateCookieStorage', [
           return $cookieStore.get(name);
         },
         set: function (name, value) {
+          $cookieStore.put(name, value);
+        },
+        put: function (name, value) {
           $cookieStore.put(name, value);
         }
       };
